ef15a0233af0bee9c2fbc6fea8f612df223ac694
Craig Frayne <craig.frayne@aboutyou.com>
2023-05-03 12:26:26 +0200

    removing files

--- assets/js/60a59c1f.6fb04159.js
+++
@@ -1,1 +0,0 @@
-"use strict";(self.webpackChunkmy_website=self.webpackChunkmy_website||[]).push([[4722],{5247:s=>{s.exports=JSON.parse('{"label":"docusaurus","permalink":"/releases/tags/docusaurus","allTagsPath":"/releases/tags","count":2}')}}]);
--- assets/js/f0ad3fbb.e6675e76.js
+++
@@ -1,1 +0,0 @@
-(self.webpackChunkmy_website=self.webpackChunkmy_website||[]).push([[8612],{5101:()=>{},2116:()=>{},4904:()=>{},6918:()=>{},4819:()=>{},3197:()=>{},996:()=>{}}]);
